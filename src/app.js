4ddbc741fc4ecbbc116cdfd9e51bc131e386b226
Amanda <acbhershey@gmail.com>
2019-08-08 00:12:39 -0400

    Played around with es6 export and import

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-console.log("Is this working?????");
+// //import './utils.js';
+// import { square, add } from './utils.js';
+//
+// console.log("app.js is working");
+// console.log(square(4));
+// console.log(add(100, 23));
+
+import { isAdult, canDrink } from './person.js';
+
+console.log(isAdult(17));
+console.log(isAdult(20));
+console.log(canDrink(20));
+console.log(canDrink(25));
---
+++ src/person.js
@@ -0,0 +1,6 @@
+// true if adult, otherwise false
+const isAdult = (age) => age >= 18;
+// true if 21 and older, otherwise false
+const canDrink = (age) => age >= 21;
+
+export { isAdult, canDrink };
