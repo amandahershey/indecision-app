9c61f4e2f665bdb95f3029af9fe44ab59ff35b1d
Amanda <acbhershey@gmail.com>
2019-08-08 02:33:20 -0400

    Exploring passing children to component (props.children)

--- src/app.js
+++ src/app.js
@@ -2,4 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+const Layout = (props) => {
+  return (
+    <div>
+      <p>Header</p>
+      {props.children}
+      <p>Footer</p>
+    </div>
+  );
+};
+
+const template = (
+  <div>
+    <h1>Page Title</h1>
+    <p>This is my page</p>
+  </div>
+);
+
+ReactDOM.render((
+  <Layout>
+    <div>
+      <h1>Page Title</h1>
+      <p>This is my page</p>
+    </div>
+  </Layout>
+), document.getElementById('app'));
