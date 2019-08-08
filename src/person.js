be9a7845a26ab276bbda58e4b9047d2f5aa98079
Amanda <acbhershey@gmail.com>
2019-08-08 01:09:04 -0400

    Copied code from prev file to new app.js file to separate components

--- src/person.js
+++
@@ -1,8 +0,0 @@
-// true if adult, otherwise false
-const isAdult = (age) => age >= 18;
-// true if 21 and older, otherwise false
-const canDrink = (age) => age >= 21;
-
-export default (age) => age >= 65;
-
-export { isAdult, canDrink };
