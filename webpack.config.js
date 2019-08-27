5872311477996c6ee15ffba56da2c616917a1c0e
Amanda <acbhershey@gmail.com>
2019-08-26 20:34:42 -0400

    Updated react-dom to 16.9.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,28 @@
 const path = require('path');
 
-module.exports = (env) => {
-  console.log('env', env);
-  return{
-    entry: "./src/app.js",
-    output: {
-      path: path.join(__dirname, 'public'),
-      filename: 'bundle.js'
-    },
-    mode: 'none',
-    module: {
-      rules: [{
-        loader: 'babel-loader',
-        test: /\.js$/,
-        exclude: /node_modules/
-      },{
-        test: /\.s?css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-      contentBase: path.join(__dirname, 'public')
-    }
-  };
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  mode: 'none',
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/,
+      exclude: /node_modules/
+    },{
+      test: /\.s?css$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
+    }]
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  }
 };
