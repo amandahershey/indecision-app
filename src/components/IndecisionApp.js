e84de80b084e0d95b5c2db4af80ac89b47f7e7e1
Amanda <acbhershey@gmail.com>
2019-08-26 22:19:10 -0400

    Set up production build w/webpack

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -53,7 +53,7 @@ export default class IndecisionApp extends React.Component{
   componentWillUnmount = () => console.log("componentWillUnmount");
 
   render(){
-    const subtitle = 'Trust your heart. Let fate decide.';
+    const subtitle = 'Put your life in the hands of a computer';
 
     return (
       <div>
